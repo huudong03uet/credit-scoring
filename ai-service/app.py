4f30449d9f2258bbd8a5bb18a889b0cc815e3848
huudong03uet <huudong03uet@gmail.com>
2025-06-14 17:21:42 +0700

    update

--- ai-service/app.py
+++ ai-service/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import random
+import uvicorn
 
 class Item(BaseModel):
     user: str
@@ -10,3 +11,6 @@ app = FastAPI()
 @app.post('/predict')
 def predict(item: Item):
     return {'score': random.randint(300,850)}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
